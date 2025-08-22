50eb2e32ecbf9202f6483c429dfa4a99ce097e5d
Brandon Ewing <brandon.ewing@warningg.com>
2025-08-22 14:57:32 -0500

    Update to newer pydantic version

--- schedule_bridge/config.py
+++ schedule_bridge/config.py
@@ -1,12 +1,16 @@
-from pydantic import (
+from typing import Any
+
+from pydantic_settings import (
     BaseSettings,
 )
+from pydantic_core import CoreSchema
+from pydantic import GetJsonSchemaHandler
 from semver.version import Version
 
 
 class PydanticVersion(Version):
     @classmethod
-    def _parse(cls, version):
+    def _parse(cls, version, optional_minor_and_patch: bool = False):
         return cls.parse(version)
 
     @classmethod
@@ -15,15 +19,19 @@ class PydanticVersion(Version):
         yield cls._parse
 
     @classmethod
-    def __modify_schema__(cls, field_schema):
+    def __get_pydantic_json_schema__(
+        cls, core_schema: CoreSchema, handler: GetJsonSchemaHandler
+    ) -> dict[str, Any]:
+        json_schema = handler.resolve_ref_schema(core_schema)
         """Inject/mutate the pydantic field schema in-place."""
-        field_schema.update(
+        json_schema.update(
             examples=[
                 "1.0.2",
                 "2.15.3-alpha",
                 "21.3.15-beta+12345",
             ]
         )
+        return json_schema
 
 
 __version__ = "0.1.0-alpha"
@@ -33,7 +41,7 @@ class Settings(BaseSettings):
     PROJECT_TITLE: str = "schedule-bridge"
     DEBUG: bool = False
     VERSION: PydanticVersion = __version__  # type:ignore
-    API_PREFIX = "/api"
+    API_PREFIX: str = "/api"
 
     class Config:
-        env_prefix = "SCHEDULE_BRIDGE"
+        env_prefix: str = "SCHEDULE_BRIDGE"
